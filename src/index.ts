5216771317e68ea24ed3d24af484d9fd558cb35d
Dario Crosa <darcros7@gmail.com>
2020-10-18 11:57:25 +0200

    telegram bot boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import * as dotenv from 'dotenv';
+import { Telegraf } from "telegraf";
+import { requireEnv } from './util';
+
+// Carica variabili d'ambiente da .env
+dotenv.config();
+
+const bot = new Telegraf(requireEnv('TELEGRAM_TOKEN'));
+
+bot.start(ctx => ctx.reply('Bot avviato!'));
+bot.command('hello', ctx => ctx.reply('world'));
+
+bot.launch()
+  .then(() => console.info('bot connesso'));
---
+++ src/util.ts
@@ -0,0 +1,23 @@
+function fixMultinePadding(s: string) {
+  return s.split('\n')
+    .map(s => s.trim())
+    .join('\n');
+}
+
+export function requireEnv(name: string) {
+  const envVar = process.env[name];
+
+  if (!envVar) {
+    const message = `
+      VARIBILI D'AMBIENTE MANCANTI!
+      Questa variabile d'ambiente non è stata settata: ${name}.
+      Puoi settarla in due modi:
+      1. crea un file chiamato .env nella stessa cartella di package.json e aggiungi questa riga "${name}=valore"
+      2. settala tramite il tuo sistema operativo (con bash, CMD, ecc..)
+    `;
+
+    throw new Error(fixMultinePadding(message));
+  }
+
+  return envVar;
+}
